7277db62e792c3f4580a09980f088668fc5c7e65
michellebridges <michellebridges727@hotmail.com>
2016-10-18 15:21:37 -0700

    In lab learning how to add functions to boxes

--- lab/dom/js/main.js
+++ lab/dom/js/main.js
@@ -6,3 +6,33 @@ var boxOne = document.getElementById('one'),
 	boxFour = document.getElementById('four'),
 	boxFive = document.getElementById('five'),
 	boxSix = document.getElementById('six');
+
+boxOne.addEventListener("click", function() {
+	boxOne.classList.add("fade-to-black");
+});
+
+boxTwo.addEventListener("click", function() {
+	boxTwo.classList.add("fall");
+});
+
+boxThree.addEventListener("mouseover", function() {
+	boxThree.classList.add("fade-to-white");
+	boxThree.classList.add("white");
+});
+
+boxThree.addEventListener("mouseout", function() {
+	boxThree.classList.remove("white");
+	boxThree.classList.add("grey");
+	boxThree.classList.add("fade-to-bg-color");
+});
+
+boxFour.addEventListener("click", function() {
+	boxFour.classList.toggle("fall");
+});
+
+boxFive.addEventListener("click", function() {
+	setInterval(function() {
+		boxFive.style.width = boxFive.offsetWidth + 10 +"px";
+		boxFive.style.height = boxFive.offsetHeight + 10 +"px";
+	}, 10);
+});
